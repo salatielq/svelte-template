7c6d7b9e0e76697c34cb1943a7b5401370f6f5c4
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2019-08-26 18:37:37 -0300

    feat: initial webpack setup to run a svelte app

---
+++ src/app/index.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let name
+</script>
+
+<style>
+  .h1 {
+    color: blue;
+  }
+</style>
+
+<h1 class="h1">Hello world! {name}</h1>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import App from './app'
+
+const app = new App({
+  target: document.body,
+  props: {
+    name: 'Salatiel',
+  }
+})
+
+export default app
